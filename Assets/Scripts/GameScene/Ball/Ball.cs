48f5931d7c706b1f77a3b93d1d4d58f3701f4b95
이홍준 <dlghdwns0219@gmail.com>
2023-09-08 21:09:57 +0900

    [LHJ_FIX] Paddle, Ball
    붙이는 기능, 클릭시 발사 기능

--- Assets/Scripts/GameScene/Ball/Ball.cs
+++ Assets/Scripts/GameScene/Ball/Ball.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Ball : MonoBehaviour
 {
     const float C_RADIAN = 180f;
     public Rigidbody2D rigidbody;
-    private float speed = 5.0f;
+    public Rigidbody2D paddle;
+    private float _speed = 5.0f;
+    private bool _isShoot = false;
 
     void Start()
     {
@@ -14,12 +17,22 @@ public class Ball : MonoBehaviour
     }
     void FixedUpdate()
     {
-        Vector3 pos = rigidbody.position;
-        Vector3 movePos = pos + transform.up * speed * Time.deltaTime;
-        rigidbody.MovePosition(movePos);
+        if (_isShoot == true)
+        {
+            Vector3 pos = rigidbody.position;
+            Vector3 movePos = pos + transform.up * _speed * Time.deltaTime;
+            rigidbody.MovePosition(movePos);
+        }
+        else
+        {
+            rigidbody.transform.position = paddle.transform.position + new Vector3(0, 0.13f, 0);
+            if (Input.GetMouseButtonUp(0))
+                _isShoot = true;
+        }
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        // collision.collider.transform.position
         if (collision.collider.CompareTag("TopWall"))
         {
             Vector3 tmp = transform.eulerAngles;
@@ -31,6 +44,13 @@ public class Ball : MonoBehaviour
             Vector3 tmp = transform.eulerAngles;
             tmp.z = (C_RADIAN * 2) - tmp.z;
             transform.eulerAngles = tmp;
+        }
+        else if (collision.collider.CompareTag("Brick"))    // 미완성
+        {
+            Vector3 tmp = transform.eulerAngles;
+            tmp.z = (C_RADIAN * 2) - tmp.z;
+            transform.eulerAngles = tmp;
+            // 벽돌 부숴지는 코드
         }
     }
 }
--- Assets/Scripts/GameScene/Paddle/Paddle.cs
+++ Assets/Scripts/GameScene/Paddle/Paddle.cs
@@ -8,40 +8,40 @@ public class Paddle : MonoBehaviour
     public Rigidbody2D rigidbody;
 
     const float C_RADIAN = 180f;
-    private float rotationX;
-    private float speed = 3.0f;
-    private float[] shootAngles = { -60, -45, -30, -15, 15, 30, 45, 60 };
+    private float _rotationX;
+    private float _speed = 3.0f;
+    private float[] _shootAngles = { -60, -45, -30, -15, 15, 30, 45, 60 };
 
     public KeyCode Left;
     public KeyCode Right;
 
-    private Vector3 startPosition;
     void Start()
     {
-        startPosition = transform.position;
         rigidbody = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        rotationX = 0f;
-        if (Input.GetKey(Left)) { rotationX -= 1f; }
-        if (Input.GetKey(Right)) { rotationX += 1f; }
-        rigidbody.velocity = new Vector3(rotationX * speed, 0, 0);
+        _rotationX = 0f;
+        if (Input.GetKey(Left)) { _rotationX -= 1f; }
+        if (Input.GetKey(Right)) { _rotationX += 1f; }
+        rigidbody.velocity = new Vector3(_rotationX * _speed, 0, 0);
     }
+ 
     private void OnCollisionEnter2D(Collision2D collision)      // 패들에 닿을때마다 무작위 각도 발사
     {
         if (collision.collider.CompareTag("Ball"))
         {
             Vector3 tmp = collision.transform.eulerAngles;
-            int r = Random.Range(0, shootAngles.Length);
-            tmp.z = shootAngles[r];
+            int r = Random.Range(0, _shootAngles.Length);
+            tmp.z = _shootAngles[r];
             collision.transform.eulerAngles = tmp;
         }
         // if (collision.tag == "Item")
     }
+ 
     /*
-    private void OnCollisionEnter2D(Collision2D collision)      // 한번 발사된 각도로 무한 고정됨
+     * private void OnCollisionEnter2D(Collision2D collision)      // 한번 발사된 각도로 무한 고정됨
     {
         if(collision.collider.CompareTag("Ball"))
         {
