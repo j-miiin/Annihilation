7c649dfe549c851170abe054e9d73432805232c8
ClassOfficer <dhdl677@gmail.com>
2023-09-10 14:45:42 +0900

    [LSY_ADD]StartSceneMake

---
+++ Assets/Scenes/lsy/Scripts/MainUI.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum ButtonType
+{
+    Start,
+    Option,
+    Exit,
+    Back
+}
+
+
+public class MainUI : MonoBehaviour
+{
+    
+}
