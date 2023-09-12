0f763037a105b9c7fc4eea2efedcff0f3a4a835a
Nwjwifh <111439484+Nwjwifh@users.noreply.github.com>
2023-09-12 17:40:42 +0900

    [KNW_ADD] 파티클 충돌 연결

--- Assets/Scripts/GameScene/Paddle/PaddleHealth.cs
+++ Assets/Scripts/GameScene/Paddle/PaddleHealth.cs
@@ -3,27 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PaddleHealth : MonoBehaviour
+public class PlayerHealth : MonoBehaviour
 {
     public int playerHealth;
 
     [SerializeField] private Image[] hearts;
+    [SerializeField] private Color invincibleColor;
+    [SerializeField] private int meteorDamage;
+    [SerializeField] private float invincibleTime = 3f;
+
+    private bool isInvincible = false;
+
+    private SpriteRenderer playerSpriteRenderer;
 
     private void Start()
     {
+        playerSpriteRenderer = GetComponent<SpriteRenderer>();
         UpdateHealth();
     }
 
     public void UpdateHealth()
     {
-        if(playerHealth <= 0)
+        if (playerHealth <= 0)
         {
-            //스테이지 실패
+            // 스테이지 실패
         }
 
         for (int i = 0; i < hearts.Length; i++)
         {
-            if(i < playerHealth)
+            if (i < playerHealth)
             {
                 hearts[i].color = Color.red;
             }
@@ -32,5 +40,47 @@ public class PaddleHealth : MonoBehaviour
                 hearts[i].color = Color.black;
             }
         }
+    }
+
+    public void PaddleInvincible()
+    {
+        isInvincible = true;
+        StartCoroutine(PaddleNotInvincible());
+
+        if (playerSpriteRenderer != null)
+        {
+            playerSpriteRenderer.color = invincibleColor;
+        }
+    }
+
+    private IEnumerator PaddleNotInvincible()
+    {
+        yield return new WaitForSeconds(invincibleTime);
+        isInvincible = false;
+
+        if (playerSpriteRenderer != null)
+        {
+            playerSpriteRenderer.color = Color.black;
+        }
+    }
+
+    public bool IsInvincible()
+    {
+        return isInvincible;
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        if (!IsInvincible())
+        {
+            TakeDamage(meteorDamage);
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        playerHealth -= damage;
+        UpdateHealth();
+        PaddleInvincible();
     }
 }
---
+++ Assets/Scripts/GameScene/UI/UIHealth.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIHealth : MonoBehaviour
+{
+    [SerializeField] private Image[] hearts;
+
+    //public void Update()
+    //{
+    //    for (int i = 0; i < hearts.Length; i++)
+    //    {
+    //        if (i < playerHealth)
+    //        {
+    //            hearts[i].color = Color.red;
+    //        }
+    //        else
+    //        {
+    //            hearts[i].color = Color.black;
+    //        }
+    //    }
+    //}
+}
