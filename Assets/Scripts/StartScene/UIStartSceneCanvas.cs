9444f2f5e153aea31d7c440f82eddb49eb28b41c
j-miiin <62470991+j-miiin@users.noreply.github.com>
2023-09-13 17:13:45 +0900

    [KJM_UPDATE] StartScene SoundSlider UI 변경

--- Assets/Scripts/StartScene/UIStartSceneCanvas.cs
+++ Assets/Scripts/StartScene/UIStartSceneCanvas.cs
@@ -6,18 +6,26 @@ using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class UIStartSceneBtn : MonoBehaviour
+public class UIStartSceneCanvas : MonoBehaviour
 {
     [SerializeField] private Button _startBtn;
     [SerializeField] private Button _optionBtn;
     [SerializeField] private Button _closeBtn;
     [SerializeField] private GameObject _optionMenu;
+    [SerializeField] private Slider _soundSlider;
+    [SerializeField] private Image _soundIcon;
 
     private void Awake()
     {
         _startBtn.onClick.AddListener(() => { GoStageScene(); });
         _optionBtn.onClick.AddListener(() => { SetOptionMenuActive(true); });
         _closeBtn.onClick.AddListener(() => { SetOptionMenuActive(false); });
+    }
+
+    private void Update()
+    {
+        if (_soundSlider.value == 0) _soundIcon.color = Color.black;
+        else _soundIcon.color = Color.white;
     }
 
     public void GoStageScene()
